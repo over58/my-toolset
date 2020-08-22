f6773f10300397ebddc6c73ad57a499ba8ca92d3
xuyongchao <xuyongchao6585@ipalfish.com>
2020-08-22 23:47:15 +0800

    feat: 移除不必要的功能

--- commands/open.js
+++ commands/open.js
@@ -2,28 +2,13 @@ const  vscode = require('vscode')
 const path = require('path')
 
 module.exports =  function addCommand (context) {
-  let config = vscode.workspace.getConfiguration('toolset')
-  if (config.get('revealFileInCode')) {
-    // code中新窗口形式打开当前文件夹
-    let disposable = vscode.commands.registerCommand('toolset.revealFileInCode', function () {
-      let fileDir = path.dirname(vscode.window.activeTextEditor.document.fileName)
-      let uri = vscode.Uri.file(fileDir)
-      vscode.commands.executeCommand('vscode.openFolder', uri, true)
-    });
-  
-    context.subscriptions.push(disposable);
-  }
+  // 资源管理中打开当前文件夹
+  let disposable2 = vscode.commands.registerCommand('toolset.revealFileInOS', function () {
+    let fileDir = path.dirname(vscode.window.activeTextEditor.document.fileName)
+    let uri = vscode.Uri.file(fileDir)
+    vscode.commands.executeCommand('revealFileInOS', uri, true)
+  });
 
-  let revealFileInOS = config.get('revealFileInOS') || true
-  if (revealFileInOS) {
-    // 资源管理中打开当前文件夹
-    let disposable2 = vscode.commands.registerCommand('toolset.revealFileInOS', function () {
-      let fileDir = path.dirname(vscode.window.activeTextEditor.document.fileName)
-      let uri = vscode.Uri.file(fileDir)
-      vscode.commands.executeCommand('revealFileInOS', uri, true)
-    });
-  
-    context.subscriptions.push(disposable2);
-  }
+  context.subscriptions.push(disposable2);
 
 }
